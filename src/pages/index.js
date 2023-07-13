cf9ee18890733fbb43567ecb1ec49b1c748c8539
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-13 11:50:16 +0200

    Added Navbar & Footer components

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <p>Hi</p>
+      <main className="min-h-screen">
+        <p className="pt-30 text-center font-bold text-3xl">Hi</p>
       </main>
     </>
   );
