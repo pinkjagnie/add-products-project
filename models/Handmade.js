deee6cbc363a143af177a3eac3edc82ba78e5e39
pinkjagnie <olga.krzysztofik@gmail.com>
2023-07-17 22:09:48 +0200

    Updated Handmade schema

--- models/Handmade.js
+++ models/Handmade.js
@@ -1,19 +1,30 @@
 import mongoose from "mongoose";
 
-const Handmade = new mongoose.Schema({
-  id: { type: Number, required: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true, min: [1, "Price must be 1 or above"] },
-  discountPercentage: { type: Number, required: true },
-  rating: { type: Number, required: true },
-  stock: { type: Number, required: true, min: [1, "Stock must be 1 or above"] },
-  owner_id: { type: Number, required: true },
-  author: { type: String, required: true },
-  tags: { type: Array, required: true },
-  thumbnail: { type: String, required: true },
-  images: { type: Array, required: true },
-});
+const Handmade = new mongoose.Schema(
+  {
+    id: { type: Number, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: {
+      type: Number,
+      required: true,
+      min: [1, "Price must be 1 or above"],
+    },
+    discountPercentage: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    stock: {
+      type: Number,
+      required: true,
+      min: [1, "Stock must be 1 or above"],
+    },
+    owner_id: { type: Number, required: true },
+    author: { type: String, required: true },
+    tags: { type: Array, required: true },
+    thumbnail: { type: String, required: true },
+    images: [{ type: String, required: true }],
+  },
+  { timestamps: true }
+);
 
 module.exports =
   mongoose.models.Handmade || mongoose.model("Handmade", Handmade);
